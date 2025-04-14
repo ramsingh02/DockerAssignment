0b226c1fbaed39587968cffd71813d1bf0a65c27
ramsingh02 <ram.singh01@nagarro.com>
2025-04-14 17:33:59 +0530

    Model and Repo

--- src/main/java/com/docker/github/actions/model/User.java
+++ src/main/java/com/docker/github/actions/model/User.java
@@ -1,2 +1,29 @@
-package com.docker.github.actions.model;public class User {
+package com.docker.github.actions.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 }
---
+++ src/main/java/com/docker/github/actions/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.docker.github.actions.repository;
+
+import com.docker.github.actions.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
