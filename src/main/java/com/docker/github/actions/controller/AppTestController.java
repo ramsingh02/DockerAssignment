6088197bde649e9840caa000d71997addf451fcb
ramsingh02 <ram.singh01@nagarro.com>
2025-04-14 13:51:53 +0530

    API Test

--- src/main/java/com/docker/github/actions/controller/AppTestController.java
+++ src/main/java/com/docker/github/actions/controller/AppTestController.java
@@ -3,16 +3,18 @@ package com.docker.github.actions.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/app")
 public class AppTestController {
 
     private final static Logger LOGGER =
             LoggerFactory.getLogger(AppTestController.class);
 
+    @GetMapping("/test")
     public ResponseEntity<String> appTest(){
         LOGGER.info("CONTROLLER : AppTestController : appTest...!!!");
 
