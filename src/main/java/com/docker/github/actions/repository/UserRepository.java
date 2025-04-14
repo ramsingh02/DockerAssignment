94cbe37de198a9dc6ceeac096473c0bb46019d5a
ramsingh02 <ram.singh01@nagarro.com>
2025-04-14 17:25:48 +0530

    CI CD TEST

--- src/main/java/com/docker/github/actions/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.docker.github.actions.repository;
-
-
-import com.docker.github.actions.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
