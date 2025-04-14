571b07cef37fed8ac3be1c91fca8e2112a63c80e
ramsingh02 <ram.singh01@nagarro.com>
2025-04-14 17:55:33 +0530

    Test

--- src/main/java/com/docker/github/actions/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docker.github.actions.repository;
-
-import com.docker.github.actions.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
